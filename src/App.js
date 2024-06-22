5141cc74224d04db7f1ebbfb30acbac24b8654e5
jxeer <julian.xeer@gmail.com>
2024-06-21 21:24:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [articles, setArticles] = useState([]);
+
+  console.log(articles);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  return (
+    <>
+      <h1>RSS Feed</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express from "express";
+import RSSParser from "rss-parser";
+
+const feedURL = "https://netflixtechblog.com/feed";
+const parser = new RSSParser();
+let articles = [];
+
+const parse = async (url) => {
+  const feed = await parser.parseURL(url);
+
+  console.log(feed.title);
+
+  feed.items.forEach((item) => {
+    articles.push({ item });
+  });
+};
+
+parse(feedURL);
+
+let app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(articles);
+});
+
+const server = app.listen("4000", () => {
+  console.log("App is lisetning at http://localhost:4000");
+});
+
+export default server;
